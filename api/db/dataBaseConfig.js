0aa1f21cb610a8f9e52e3556bb36d91ca4451219
gitEllE_if <elvira_lina@mail.ru>
2021-04-25 08:12:06 +0300

    lesson 8

---
+++ api/db/dataBaseConfig.js
@@ -0,0 +1,10 @@
+const DB_CONFIG = {
+    host: "localhost",
+    user: "root",
+    password: "pass",
+    database: "chat_db",
+    tableUsers: "users",
+    tableChat: "chat"
+}
+
+module.exports = DB_CONFIG;
